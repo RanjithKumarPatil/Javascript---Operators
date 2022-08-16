1fc7905aba24b3f8a52b1de2709b12820d2ce93f
RanjithKumar <ranjithkumarpatil@gmail.com>
2022-08-16 22:57:53 +0530

    operators practice

---
+++ arithematic_operators.js
@@ -0,0 +1,16 @@
+var a = 35;
+var b = 6;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+var c = 42;
+c++;
+var c1 = c;
+console.log(c1);
+b--;
+var d = b;
+console.log(d);
---
+++ comparison_operators.js
@@ -0,0 +1,6 @@
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
